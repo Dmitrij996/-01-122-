a1418f617540b07737dae1ed6f081d52e0281e2c
Виктория <vikakononova66@mail.ru>
2025-03-23 16:41:39 +0500

    Обновление 0.19

---
+++ script.js
@@ -0,0 +1,17 @@
+function onEntry(entry){
+    entry.forEach(change =>{
+        if (change.isIntersecting){
+            change.target.classList.add(
+                'animation'
+            );
+        }
+    });
+}
+let options = {
+    threshold:[0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('article');
+for(let elm of elements){
+    observer.observe(elm)
+};
