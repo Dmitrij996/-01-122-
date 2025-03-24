d3e4b8a3532f3d2c1069b92d76e8cf5c558718de
Виктория <vikakononova66@mail.ru>
2025-03-24 13:30:28 +0500

    Обновление 0.21

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ for(let elm of elements){
     observer.observe(elm)
 };
 
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    let slides = document.getElementsByClassName("slides");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+let lastScroll = 0;
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll) {
+        // Прокрутка вниз'
+        header.style.top = '-1000px';
+    } else {
+        // Прокрутка вверх
+        header.style.top = '0';
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000);
-}
+    lastScroll = currentScroll;
+});
